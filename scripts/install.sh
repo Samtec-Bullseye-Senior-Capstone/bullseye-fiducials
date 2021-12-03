4fadb275b40508a2f87ce6c68db3d40c0d8bb3f6
Connor Peskin <peskin.12@gmail.com>
2021-12-03 14:14:44 +0000

    added install script for docker builds

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR=`dirname "$(readlink -f "$0")"` 
 BASE_DIR=`dirname "$SCRIPT_DIR"`
 
-rm -r $BASE_DIR/build && $SCRIPT_DIR/build.sh
+rm -rf $BASE_DIR/build && $SCRIPT_DIR/build.sh
 cp $BASE_DIR/build/bullseye-fiducials /bin/bullseye-fiducials

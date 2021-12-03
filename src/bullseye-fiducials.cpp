b9a9703ce52e87b0cd68d275f22325130a495893
Connor Peskin <peskin.12@gmail.com>
2021-12-03 22:21:48 +0000

    JSON output

--- src/bullseye-fiducials.cpp
+++ src/bullseye-fiducials.cpp
@@ -22,10 +22,21 @@
 
 #include <opencv2/highgui/highgui.hpp> // imread
 #include <opencv2/imgproc.hpp> 
+#include <nlohmann/json.hpp>
 
 #include <iostream>
+#include <string>
 using std::cout;
+using json = nlohmann::json;
 
+json cornersJSON ( cv::Matx<float, 4, 2> corners ) {
+	json j;
+	j["top_left"] = { {"x", corners(0,0)}, {"y", corners(0,1)} };
+	j["top_right"] = { {"x", corners(1,0)}, {"y", corners(1,1)} };
+	j["bottom_left"] = { {"x", corners(3,0)}, {"y", corners(3,1)} };
+	j["bottom_right"] = { {"x", corners(2,0)}, {"y", corners(2,1)} };
+	return j;
+}
 
 int main(int argc, char* argv[])
 {
@@ -39,38 +50,39 @@ int main(int argc, char* argv[])
 
     cv::Mat image = cv::imread(argv[1]);
     if(image.data) {
-		cout << "Rows, Cols: " << image.rows << ", " << image.cols << std::endl;
+		json j;
         for (const auto &tag : chilitags::Chilitags().find(image)) {
-            cout << tag.first << "\n";
-			cout << "tag.second = " << "\n" << " "  << tag.second << "\n" << "\n";
-			cv::line(image, 
-					cv::Point(tag.second(0,0), tag.second(0,1)),
-				   	cv::Point(tag.second(1,0), tag.second(1,1)), 
-					cv::Scalar(255,0,0), 
-					thickness, 
-					cv::LINE_4);
-			cv::line(image, 
-					cv::Point(tag.second(0,0), tag.second(0,1)),
-				   	cv::Point(tag.second(3,0), tag.second(3,1)), 
-					cv::Scalar(255,0,0), 
-					thickness, 
-					cv::LINE_4);
-			cv::line(image, 
-					cv::Point(tag.second(1,0), tag.second(1,1)),
-				   	cv::Point(tag.second(2,0), tag.second(2,1)), 
-					cv::Scalar(255,0,0), 
-					thickness, 
-					cv::LINE_4);
-			cv::line(image, 
-					cv::Point(tag.second(2,0), tag.second(2,1)),
-				   	cv::Point(tag.second(3,0), tag.second(3,1)), 
-					cv::Scalar(255,0,0), 
-					thickness, 
-					cv::LINE_4);
+			j[std::to_string(tag.first)] = cornersJSON(tag.second);
+            // cout << tag.first << "\n";
+			// cout << "tag.second = " << "\n" << " "  << tag.second << "\n" << "\n";
+			// cv::line(image, 
+			// 		cv::Point(tag.second(0,0), tag.second(0,1)),
+			// 	   	cv::Point(tag.second(1,0), tag.second(1,1)), 
+			// 		cv::Scalar(255,0,0), 
+			// 		thickness, 
+			// 		cv::LINE_4);
+			// cv::line(image, 
+			// 		cv::Point(tag.second(0,0), tag.second(0,1)),
+			// 	   	cv::Point(tag.second(3,0), tag.second(3,1)), 
+			// 		cv::Scalar(255,0,0), 
+			// 		thickness, 
+			// 		cv::LINE_4);
+			// cv::line(image, 
+			// 		cv::Point(tag.second(1,0), tag.second(1,1)),
+			// 	   	cv::Point(tag.second(2,0), tag.second(2,1)), 
+			// 		cv::Scalar(255,0,0), 
+			// 		thickness, 
+			// 		cv::LINE_4);
+			// cv::line(image, 
+			// 		cv::Point(tag.second(2,0), tag.second(2,1)),
+			// 	   	cv::Point(tag.second(3,0), tag.second(3,1)), 
+			// 		cv::Scalar(255,0,0), 
+			// 		thickness, 
+			// 		cv::LINE_4);
 		}
-		cv::imwrite("out.png", image);
+		cout << j << std::endl;
+		// cv::imwrite("out.png", image);
         return 0;
     }
-	cout << "nah fam" << std::endl;
     return 1;
 }
